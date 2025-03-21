ec3746a45e84b36951798463251ccfb2442fdbfe
Sanjita Ray <sanjitaray62@gmail.com>
2025-03-21 22:50:53 +0530

    edited

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 
 //Index Route
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapasync(  async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
-});
+}));
 
 //New Route
 app.get("/listings/new", (req, res) => {
@@ -44,43 +44,49 @@ app.get("/listings/new", (req, res) => {
 });
 
 //Show Route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapasync( async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
-});
+}));
 
 //Create Route
 app.post("/listings", wrapasync( async (req, res,next) => {
-
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-
-  
+  if(!req.body.listing){
+    throw new ExpressError(400, "Send valid data for listing");
+  }
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 }));
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapasync( async (req, res) => {
+  if(!req.body.listing){
+    throw new ExpressError(400, "Send valid data for listing");
+  }
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/edit.ejs", { listing });
-});
+}));
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapasync( async (req, res) => {
+  if(!req.body.listing){
+    throw new ExpressError(400, "Send valid data for listing");
+  }
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id",wrapasync(  async (req, res) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
   res.redirect("/listings");
-});
+}));
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
@@ -96,9 +102,13 @@ app.delete("/listings/:id", async (req, res) => {
 //   res.send("successful testing");
 // });
 
+app.all("*", (req, res, next)=>{
+  next(new ExpressError(404, "page not found!"));
+});
+
 //Middle Ware
 app.use((err, req, res, next )=>{
-  let {statusCode , message} = err;
+  let {statusCode=500 , message="Something Went Wrong"} = err;
   res.status(statusCode).send(message);
 });
 
--- utils/wrapasync.js
+++ utils/wrapasync.js
@@ -1,4 +1,4 @@
-mmodule.exports = (fn) => {
+module.exports = (fn) => {
     return (req, res, next) => {
         fn(req, res, next).catch(next);
     }
